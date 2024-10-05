222160040753a35ca401355bc37d7291e18706ce
João Marcos <jmarcosmmn@gmail.com>
2024-10-05 09:59:00 -0300

    feat: creating sub items

--- src/components/order/orderItems/Items.tsx
+++ src/components/order/orderItems/Items.tsx
@@ -1,55 +1,54 @@
 import React from "react";
 import menuItems from "../../../app/data/menuItems.json";
-import { Button } from "@mui/material";
+import { ListItemText, ListItemButton, ListItemIcon, List, Collapse } from "@mui/material";
 import { useState } from "react";
+import { ExpandMore, ExpandLess, AddBoxTwoTone, StarBorder } from '@mui/icons-material'; // {{ edit_2 }}
 
 export default function Items() {
-  const [showSubItems, setShowSubItems] = useState(false);
 
-  console.log(showSubItems);
+  const [openIndex, setOpenIndex] = useState(null); // Armazena o índice do item aberto
+
+  const handleClick = (index) => {
+    setOpenIndex(openIndex === index ? null : index); // Fecha se já estiver aberto
+  };
 
   return (
     <div className="grid grid-cols-2 gap-4">
 
-        {menuItems["menu"].map((item) => (
-          <div key={item.category} className={showSubItems ? "col-span-2" : ""}>
-          <Button
-              sx={{ padding: "20px", backgroundColor: "#8a633d", width: '100%'}}
-              variant="contained"
-              onClick={() => setShowSubItems(!showSubItems)}
-              key={item.category}
-            >
-              {item.category}
-          </Button>
-          </div>  
+      {menuItems["menu"].map((item, index) => (
+
+        <List key={item.category} >
+          <ListItemButton  sx={{backgroundColor: '#5c422799', color: '#fff',  '&:hover': {backgroundColor: '#5c4227'}}} onClick={() => handleClick(index)}>
+            <ListItemText primary={item.category} />
+            {openIndex === index ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+
+
+          {item.items.map(item => (
+
+                <Collapse key={item.name} in={openIndex === index? true : false} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                    <AddBoxTwoTone />
+                    </ListItemIcon>
+                    <ListItemText primary={item.name} />
+                  </ListItemButton>
+                </List>
+                </Collapse>
+
+
+          ))}
+
+
+        
+
+       </List>
 
       ))}
 
-      {/* <List
-                sx={{ width: '100%', maxWidth: 360 }}
-                component="nav"
-                aria-labelledby="nested-list-subheader"
-                >
-                <ListItemButton sx={{padding: 2}}>
-                    <ListItemIcon>
-                        <AddBoxTwoToneIcon sx={{color: '#4b3621'}}></AddBoxTwoToneIcon>
-                    </ListItemIcon>
-                    <ListItemText primary="Sent mail" />
-                </ListItemButton>
-                <Divider />
-                <ListItemButton>
-                <ListItemIcon>
-                        <AddBoxTwoToneIcon></AddBoxTwoToneIcon>
-                    </ListItemIcon>
-                    <ListItemText primary="Sent mail" />
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemText primary="Sent mailSent maiSent maiSent maiSent maiSent mai" />
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemText primary="Sent mail" />
-                </ListItemButton>
-                </List> */}
     </div>
+
+
   );
 }
