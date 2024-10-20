8242e989146c0b8e3632f75af8cf35f28b3614b1
João Marcos <jmarcosmmn@gmail.com>
2024-10-19 22:58:19 -0300

    fix: responsive text in OrdeList

--- src/app/components/OrderList.tsx
+++ src/app/components/OrderList.tsx
@@ -1,5 +1,5 @@
 'use client'
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { List, ListItem, ListItemText } from '@mui/material'; // Add this import
 import Counter from './Counter';
 import HandleOrderItem from './HandleOrderItem'; // Updated import to match the casing
@@ -8,29 +8,50 @@ import { useOrderContext } from '../context/OrderContext';
 function OrderList() {
 
     const { dishes } = useOrderContext();
+    const [ isMounted, setIsMounted ] = useState<boolean>(false);
+
+    useEffect(() => {setIsMounted(true)},[]) // Apenas será true no cliente, evitando discrepância entre SSR e CSR
+
+    if (!isMounted) {
+        // Evita a renderização antes que o componente esteja montado no cliente
+        return null;
+      }
 
     return (
         <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
             {dishes.map((dish, index) => (
             <ListItem
                 key={index}
-                sx={{backgroundColor: '#5c422710', marginBottom : '10px', borderRadius : '10px', width: '100%', boxShadow: '3px 3px 5px 0px #5c422730', padding : '7px' } }
+                sx={{backgroundColor: '#5c422710', marginBottom : '10px', borderRadius : '10px', width: '100%', boxShadow: '3px 3px 5px 0px #5c422730', padding : '5px' } }
                 disableGutters
             >
-                <div className='flex justify-between w-full'>
-                    <div className=''>
+                <div className='flex w-full max-w-[100%]'>
+                    <div className='flex justify-center items-center'>
                         <HandleOrderItem dishIndex={index} />
                     </div>
 
-                    <div className='flex pl-5 items-center'>
+                    <div className='flex flex-grow pl-5 pr-5 items-center flex-grow'>
                     <ListItemText primary={<span className="text-[25px] text-[#5c4227] font-bold">{dish.amount}</span>} />    
                     </div> 
 
-                    <div className='flex pl-5 items-center w-full'>
-                    <ListItemText primary={<span className="text-[20px] text-[#5c4227]">{dish.name}</span>} />    
-                    </div>    
+                    {dish.note == null ? (
+                        <div className='flex items-center flex-grow'>
+                            <ListItemText primary={<span className="text-[20px] text-[#5c4227]">{dish.name}</span>} />
+                        </div>    
+                    )
+                    :
+                        <div className="flex flex-col justify-center w-full mt-1">
+                            <ListItemText 
+                            primary={<span className="text-[20px] text-[#5c4227]">{dish.name}</span>} 
+                            />
+                            <ListItemText 
+                            primary={<div className="text-[14px] text-[#5c422799] whitespace-normal">{dish.note}</div>} 
+                            />
+                        </div>
+                
+                    }
 
-                    <div className='flex justify-end'>
+                    <div className='flex flex-grow justify-end w[40px]'>
                         <Counter containerWidth={'30px'} dishIndex={index} dishDepartiment={dish.departiment}/>  
                     </div>
                 </div>
